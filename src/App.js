94dd6068854f0bae24fe524c20bef5eceb90f13e
Ajit <ajit.peshane@HHSTechGroup.com>
2021-03-14 09:48:19 +0530

    all added

--- src/App.js
+++ src/App.js
@@ -1,24 +1,25 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import axios from "axios";
+
+const Dummydata = () => {
+  const InsideData = () => {
+    axios.get("https://jsonplaceholder.typicode.com/todos/1").then(res => {
+      console.log(res);
+    });
+  };
+  useEffect(() =>
+  {
+    InsideData();
+  }, []);
+  return <h1>Hello </h1>
+};
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Dummydata />
     </div>
   );
 }
